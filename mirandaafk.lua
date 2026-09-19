51fa5185c703617fca133c2e78ef67cbc609fb4d
miirandahub <mirandacallruim2005@gmail.com>
2026-09-19 19:14:44 -0300

    Update mirandaafk.lua

--- mirandaafk.lua
+++ mirandaafk.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/7c9325541211f50b04d7acdfab526483.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/f13173bf00a31de9a32e7d3089572f3d.lua"))()
